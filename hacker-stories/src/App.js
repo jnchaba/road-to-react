3450523bfa637685e1e6517222e3203e338ef16f
Jakub Chaba <jakub.chaba@btopenworld.com>
2021-10-12 23:09:11 -0400

    end chapter 8: handle function in JSX

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -47,11 +47,15 @@ const List = () => (
 );
 
 
-const Search = () => (
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  }
+  return (
     <div>
       <label htmlFor="search">Search</label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}/>
     </div>
-);
+)};
 
 export default App;
