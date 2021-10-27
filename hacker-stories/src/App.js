0b9d6741d1f3ef5b1fcb2effa5fe3968c8407219
jnchaba <jakub.chaba@btopenworld.com>
2021-10-27 18:47:03 -0400

    end chapter 13: react controlled components

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -26,12 +26,14 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+  const searchedStories = stories.filter(story => 
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleSearch}/>
+      <Search search ={searchTerm} onSearch={handleSearch}/>
       <hr />
       <List list={searchedStories}/>
     </div>
@@ -41,7 +43,12 @@ const App = () => {
 const Search = (props) => (
     <div>
       <label htmlFor="search">Search</label>
-      <input id="search" type="text" onChange={props.onSearch}/>
+      <input 
+        id="search"
+        type="text"
+        value={props.search}
+        onChange={props.onSearch}
+        />
     </div>
 );
 
